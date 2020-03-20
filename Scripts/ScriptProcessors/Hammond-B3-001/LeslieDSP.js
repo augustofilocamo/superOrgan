72b6ce8d08f36bb07e5b54a8b1fdc7943d38554b
ustk iMac <g.delsavio@hotmail.fr>
2020-03-20 13:38:18 +0100

    Add Leslie DSP

---
+++ Scripts/ScriptProcessors/Hammond-B3-001/LeslieDSP.js
@@ -0,0 +1,14 @@
+Engine.createDspNetwork("LeslieDSP");
+function prepareToPlay(sampleRate, blockSize)
+{
+	
+}
+ function processBlock(channels)
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
