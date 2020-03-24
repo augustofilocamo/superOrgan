f823a47f4fe6da4fc8fc612c26590487bae98698
Filo <augustofilocamo@gmail.com>
2020-03-24 16:42:30 -0300

    improved timbre

---
+++ Scripts/ScriptProcessors/Hammond-B3-002/Interface.js
@@ -0,0 +1,21 @@
+Content.makeFrontInterface(1100, 700);function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
