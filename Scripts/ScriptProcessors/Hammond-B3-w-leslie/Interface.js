b16032b7e73e7a342f15c5ec6b890025860a5638
ustk iMac <g.delsavio@hotmail.fr>
2020-03-28 15:30:21 +0100

    Leslie fix and improvement, Add a pseudo stop position

--- Scripts/ScriptProcessors/Hammond-B3-w-leslie/Interface.js
+++ Scripts/ScriptProcessors/Hammond-B3-w-leslie/Interface.js
@@ -1,4 +1,33 @@
-Content.makeFrontInterface(1100, 700);function onNoteOn()
+Content.makeFrontInterface(1100, 700);
+
+
+
+// Leslie
+const var ScriptFX1 = Synth.getEffect("Script FX1");
+
+
+inline function onleslieSpeedControl(component, value)
+{
+    switch (value){
+        
+        case 0:
+            ScriptFX1.setAttribute(ScriptFX1.Speed, 0);
+            ScriptFX1.setAttribute(ScriptFX1.BP, 0);
+            break;
+            
+        case 1:
+            ScriptFX1.setAttribute(ScriptFX1.Speed, 0);
+            ScriptFX1.setAttribute(ScriptFX1.BP, 1);
+            break;
+            
+        case 2:
+            ScriptFX1.setAttribute(ScriptFX1.Speed, 1);
+            ScriptFX1.setAttribute(ScriptFX1.BP, 0);
+            break;
+    }
+};
+Content.getComponent("leslieSpeed").setControlCallback(onleslieSpeedControl);
+function onNoteOn()
 {
 	
 }
