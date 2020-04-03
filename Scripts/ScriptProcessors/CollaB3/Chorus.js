97bf055256530e8ca3ad524e9e6a9915cba32aed
Filo <augustofilocamo@gmail.com>
2020-04-03 11:01:21 -0300

    clean up

---
+++ Scripts/ScriptProcessors/CollaB3/Chorus.js
@@ -0,0 +1,14 @@
+Engine.createDspNetwork("chorusDSP");
+function prepareToPlay(sampleRate, blockSize)
+{
+	
+}
+ function processBlock(channels)
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
